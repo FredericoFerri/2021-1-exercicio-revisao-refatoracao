fcf644614224e5116cc1f03090ba15e38df9d590
FredericoFerri <61014235+FredericoFerri@users.noreply.github.com>
2021-08-17 22:20:59 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,14 +5,19 @@
 using namespace std;
 
 class Cliente {
-    public:
-        string NOME;
+    
+    Cliente (string name, string adress, string city, string state, string cepy, string tel) :
+    nome(name), endereco(adress), cidade(city), uf(state), cep(cepy), telefone(tel) {}
+    
+    private:
+        string nome;
         string endereco;
-        string CIDADE;
-        string UF;
+        string cidade;
+        string uf;
         string cep;
         string telefone;
 
+    public:
         void print();
 };
 
